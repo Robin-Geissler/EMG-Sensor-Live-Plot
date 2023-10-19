d708768cac459f406288ca80b7240a62e1e241e0
Geißler <robin.geissler@emft.fraunhofer.de>
2023-10-19 09:53:56 +0200

    adds live graph

--- main.py
+++ main.py
@@ -4,6 +4,42 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import serial
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    # Read potentials from EMG sensor
+    potential = int.from_bytes(ser.read(4), byteorder='little', signed=True)
+    print(potential)
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%S.%f'))
+    ys.append(potential)
+    #'%H:%M:%S.%f'
+
+    # Limit x and y lists to 20 items
+    xs = xs[-200:]
+    ys = ys[-200:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    #plt.xticks(rotation=90, ha='right')
+    #plt.subplots_adjust(bottom=0.30)
+    plt.title('EMG Measurement')
+    plt.ylabel('Voltage')
+
+
+
+
+
+
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -18,11 +54,26 @@ if __name__ == '__main__':
 
     #ser = serial.Serial( port = 'COM3', baudrate = 115200, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout=2)
     ser = serial.Serial(port='COM3', baudrate=115200)
-
     print('configured')
-    while True:
-        i = int.from_bytes(ser.read(4), byteorder='little')
-        print(i)
-    ser.close()
+
+    # Create figure for plotting
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    xs = []
+    ys = []
+
+
+
+
+    # Set up plot to call animate() function periodically
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=100, save_count=200)
+    plt.show()
+
+#    while True:
+#        i = int.from_bytes(ser.read(4), byteorder='little')
+#        print(i)
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
